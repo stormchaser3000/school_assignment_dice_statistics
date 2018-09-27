d055333430223768ca3902d52bd61c08a3c1b568
Ian Sutter <stormchaser30000@gmail.com>
2018-09-27 17:07:28 -0500

    add the code

---
+++ dice_statistics.py
@@ -0,0 +1,33 @@
+import random
+
+num_sixes = 0
+num_sevens = 0
+num_rolls = int(input('Enter number of rolls:\n'))
+totals = []
+
+# define a function to get the numbers from the totals list
+def times(lst, x):
+    times = 0
+    for i in lst:
+        if i == x:
+            times += 1
+    return times
+
+if num_rolls >= 1:
+    for i in range(num_rolls):
+        die1 = random.randint(1,6)
+        die2 = random.randint(1,6)
+        roll_total = die1 + die2
+
+        # add the sum of the dice to a dictionary each roll
+        totals.append(roll_total)
+
+    max_dice_num = 12
+
+    # print the statistics
+    print("\nDice rolling statistics:")
+    for i in range(max_dice_num):
+
+        print(f"{i + 1}s: {times(totals, i)}")
+else:
+    print('Invalid number of rolls. Try again.')
